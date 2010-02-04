6a5ed42a21b71df063d5cd75b49a2812af167562
lawgon <lawgon@thenilgiris.com>
2010-02-04 12:39:41 +0530

    changesql

--- sql.sql
+++ sql.sql
@@ -2,5 +2,5 @@ BEGIN;
 
 alter table "web_matchentry" add column "tee_id" integer REFERENCES "web_tee" ("id") DEFERRABLE INITIALLY DEFERRED;
 update "web_matchentry" set tee_id = 1;
-alter table "web_matchentry" alter column tee_id set not null;
+
 end;
